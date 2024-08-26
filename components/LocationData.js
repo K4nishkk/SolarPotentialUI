e65c081df1728e44e3deb4009f43bf86324fff72
K4nishkk <k4nishkk@gmail.com>
2024-08-26 13:04:55 +0530

    testing

--- components/LocationData.js
+++ components/LocationData.js
@@ -24,10 +24,10 @@ const LocationData = () => {
             </div>
             <div className='innerbox'>
                 <div className="card">
-                    <div className="card-info">
+                    {/* <div className="card-info">
                         <div className="card-title">Current Address</div>
                         <div className="card-subtitle">UCHC Sector - 45, Sukhna Path, Sector 45, Chandigarh - 160047, CH, India</div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
