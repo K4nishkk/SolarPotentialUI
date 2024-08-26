54d3902a57d83b089da30fc39fa9f88b76ce1f0e
K4nishkk <k4nishkk@gmail.com>
2024-08-26 10:03:00 +0530

    insecure api issue fixed

--- utils/Location.js
+++ utils/Location.js
@@ -1,7 +1,8 @@
 export async function getCoordinatesFromIP() {
+
     const response = await fetch(process.env.NEXT_PUBLIC_IP_API_ENDPOINT);
     const data = await response.json();
-    return data;
+    return {lat: data.location.latitude, lon: data.location.longitude};
 }
 
 export async function getCoordinatesFromAddress(address) {
